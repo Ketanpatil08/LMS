6f76ae465a5d282018c7bcd3b0b7161f95c23595
Sushant219 <sushantkadam152@gmail.com>
2025-06-11 14:47:18 +0530

    configured database connection and fixed env variables

---
+++ index.js
@@ -0,0 +1,5 @@
+let app=require("./src/app.js");
+
+app.listen(6000,(req,res)=>{
+    console.log("Server is running on port 6000");
+});
---
+++ src/app.js
@@ -0,0 +1,14 @@
+let express=require("express");
+let session=require("express-session");
+let app=express();
+let bodyParser=require("body-parser");
+let router=require("../src/routes/routes.js");
+let conn=require("./config/db.js");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+app.use("/",router);
+app.set("view engine","ejs");
+
+module.exports=app;
